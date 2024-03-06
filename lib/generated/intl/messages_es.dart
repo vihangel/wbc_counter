19e4aaf677408c3b5fa399f2e9b6787c4385bd77
vitoria angel <vitoria.angel@dengun.com>
2024-03-06 13:49:59 -0400

    feat erb files

---
+++ lib/generated/intl/messages_es.dart
@@ -0,0 +1,213 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'es';
+
+  static String m0(totalQuantity) => "Has agregado ${totalQuantity} células";
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Other": MessageLookupByLibrary.simpleMessage("Otros"),
+        "abnormalCells":
+            MessageLookupByLibrary.simpleMessage("Células anormales"),
+        "about": MessageLookupByLibrary.simpleMessage("Acerca de"),
+        "add": MessageLookupByLibrary.simpleMessage("Agregar"),
+        "addNewAlertThreshold": MessageLookupByLibrary.simpleMessage(
+            "Agregar nuevo umbral de alerta"),
+        "age": MessageLookupByLibrary.simpleMessage("Edad"),
+        "alertThresholds":
+            MessageLookupByLibrary.simpleMessage("Notificar conteo"),
+        "alertToCount": MessageLookupByLibrary.simpleMessage(
+            "Alertar cuando el conteo de WBC alcance:"),
+        "appEvaluation":
+            MessageLookupByLibrary.simpleMessage("Evaluación de la aplicación"),
+        "basophilsDescription": MessageLookupByLibrary.simpleMessage(
+            "Los basófilos tienen un núcleo bilobulado, pero puede ser difícil de discernir debido a los gránulos densos. El citoplasma es de color azul oscuro a negro debido a los gránulos basófilos. Son similares en tamaño a los neutrófilos y eosinófilos. Tienen gránulos densos, que ocultan la visión del núcleo. Están involucrados en reacciones alérgicas graves y respuestas inmunes."),
+        "basophilsTitle": MessageLookupByLibrary.simpleMessage("Basófilos"),
+        "basophilsTopic1": MessageLookupByLibrary.simpleMessage(
+            "Núcleo: Los basófilos tienen un núcleo bilobulado, pero puede ser difícil de discernir debido a los gránulos densos."),
+        "basophilsTopic2": MessageLookupByLibrary.simpleMessage(
+            "Color del citoplasma: El citoplasma es de color azul oscuro a negro debido a los gránulos basófilos."),
+        "basophilsTopic3": MessageLookupByLibrary.simpleMessage(
+            "Tamaño y forma general: Son similares en tamaño a los neutrófilos y eosinófilos."),
+        "basophilsTopic4": MessageLookupByLibrary.simpleMessage(
+            "Gránulos citoplasmáticos: Tienen gránulos densos, que ocultan la visión del núcleo."),
+        "basophilsTopic5": MessageLookupByLibrary.simpleMessage(
+            "Movilidad y actividad: Están involucrados en reacciones alérgicas graves y respuestas inmunes."),
+        "beforeLeaving": MessageLookupByLibrary.simpleMessage("Antes de salir"),
+        "buyMeACoffe": MessageLookupByLibrary.simpleMessage("Cómprame un café"),
+        "calculate": MessageLookupByLibrary.simpleMessage("Calcular"),
+        "cancel": MessageLookupByLibrary.simpleMessage("Cancelar"),
+        "clearAll": MessageLookupByLibrary.simpleMessage("Borrar todo"),
+        "clearValues": MessageLookupByLibrary.simpleMessage("Limpiar valores"),
+        "close": MessageLookupByLibrary.simpleMessage("Cerrar"),
+        "configWbc":
+            MessageLookupByLibrary.simpleMessage("Configuración de WBC"),
+        "confirm": MessageLookupByLibrary.simpleMessage("Confirmar"),
+        "confirmDeleteReport": MessageLookupByLibrary.simpleMessage(
+            "¿Estás seguro de que quieres eliminar este informe?"),
+        "contactUs": MessageLookupByLibrary.simpleMessage("Contáctanos"),
+        "couldNotLaunch":
+            MessageLookupByLibrary.simpleMessage("No se pudo lanzar"),
+        "dark": MessageLookupByLibrary.simpleMessage("Oscuro"),
+        "delete": MessageLookupByLibrary.simpleMessage("Eliminar"),
+        "deleteReport":
+            MessageLookupByLibrary.simpleMessage("Eliminar informe"),
+        "disabled": MessageLookupByLibrary.simpleMessage("Desactivado"),
+        "discardChanges":
+            MessageLookupByLibrary.simpleMessage("Descartar cambios"),
+        "edit": MessageLookupByLibrary.simpleMessage("Editar"),
+        "enabled": MessageLookupByLibrary.simpleMessage("Activado"),
+        "english": MessageLookupByLibrary.simpleMessage("Inglés"),
+        "eosinophilsDescription": MessageLookupByLibrary.simpleMessage(
+            "Los eosinófilos tienen un núcleo bilobulado, que se asemeja a un par de gafas. El citoplasma es de color rosa-anaranjado debido a la presencia de gránulos eosinófilos. Son ligeramente más grandes que los neutrófilos y tienen una forma irregular. Los gránulos son grandes y teñidos, generalmente de color rosa. Están involucrados en la respuesta alérgica y en la defensa contra parásitos."),
+        "eosinophilsTitle": MessageLookupByLibrary.simpleMessage("Eosinófilos"),
+        "eosinophilsTopic1": MessageLookupByLibrary.simpleMessage(
+            "Núcleo: Los eosinófilos tienen un núcleo bilobulado, que se asemeja a un par de gafas."),
+        "eosinophilsTopic2": MessageLookupByLibrary.simpleMessage(
+            "Color del citoplasma: El citoplasma es de color rosa-anaranjado debido a la presencia de gránulos eosinófilos."),
+        "eosinophilsTopic3": MessageLookupByLibrary.simpleMessage(
+            "Tamaño y forma general: Son ligeramente más grandes que los neutrófilos y tienen una forma irregular."),
+        "eosinophilsTopic4": MessageLookupByLibrary.simpleMessage(
+            "Gránulos citoplasmáticos: Los gránulos son grandes y teñidos, generalmente de color rosa."),
+        "eosinophilsTopic5": MessageLookupByLibrary.simpleMessage(
+            "Movilidad y actividad: Están involucrados en la respuesta alérgica y en la defensa contra parásitos."),
+        "errorLoadingReports":
+            MessageLookupByLibrary.simpleMessage("Error al cargar informes"),
+        "explainWhySupport": MessageLookupByLibrary.simpleMessage(
+            "Tu apoyo nos ayuda a seguir desarrollando y mejorando nuestra aplicación. Con tu ayuda, podemos ofrecer actualizaciones regulares, nuevas características y asegurar que la aplicación siga siendo gratuita para todos los usuarios. ¡Gracias por apoyar nuestra misión!"),
+        "filter": MessageLookupByLibrary.simpleMessage("Filtrar"),
+        "french": MessageLookupByLibrary.simpleMessage("Francés"),
+        "howIdentify": MessageLookupByLibrary.simpleMessage(
+            "¿Cómo identificar las células?"),
+        "itHelpsALot":
+            MessageLookupByLibrary.simpleMessage("¡Nos ayuda mucho!"),
+        "italian": MessageLookupByLibrary.simpleMessage("Italiano"),
+        "language": MessageLookupByLibrary.simpleMessage("Idioma"),
+        "later": MessageLookupByLibrary.simpleMessage("Más tarde"),
+        "leaveAReview": MessageLookupByLibrary.simpleMessage(
+            "Deja una reseña en la tienda"),
+        "light": MessageLookupByLibrary.simpleMessage("Claro"),
+        "likeTheApp":
+            MessageLookupByLibrary.simpleMessage("¿Te gusta la aplicación?"),
+        "lymphocytesDescription": MessageLookupByLibrary.simpleMessage(
+            "Los linfocitos son células del sistema inmunológico. Tienen núcleos densos, redondos y uniformes. El color del citoplasma es azul claro a incoloro. Son más pequeños que los neutrófilos y generalmente tienen forma esférica. Por lo general, no contienen gránulos visibles. Menos móviles que los neutrófilos, están involucrados en la respuesta inmune adaptativa."),
+        "lymphocytesTitle": MessageLookupByLibrary.simpleMessage("Linfocitos"),
+        "lymphocytesTopic1": MessageLookupByLibrary.simpleMessage(
+            "Núcleo: Los linfocitos tienen núcleos densos, redondos y uniformes."),
+        "lymphocytesTopic2": MessageLookupByLibrary.simpleMessage(
+            "Color del citoplasma: El color del citoplasma es azul claro a incoloro."),
+        "lymphocytesTopic3": MessageLookupByLibrary.simpleMessage(
+            "Tamaño y forma general: Son más pequeños que los neutrófilos y generalmente tienen forma esférica."),
+        "lymphocytesTopic4": MessageLookupByLibrary.simpleMessage(
+            "Gránulos citoplasmáticos: Por lo general, no contienen gránulos visibles."),
+        "lymphocytesTopic5": MessageLookupByLibrary.simpleMessage(
+            "Movilidad y actividad: Menos móviles que los neutrófilos, están involucrados en la respuesta inmune adaptativa."),
+        "mode": MessageLookupByLibrary.simpleMessage("Modo"),
+        "monocytesDescription": MessageLookupByLibrary.simpleMessage(
+            "Los monocitos tienen un núcleo grande, de forma irregular y a menudo en forma de riñón. El citoplasma es gris-azulado. Son las células blancas más grandes y tienen una forma irregular. Por lo general, no tienen gránulos visibles. Son móviles y se diferencian en macrófagos cuando se desplazan a los tejidos."),
+        "monocytesTitle": MessageLookupByLibrary.simpleMessage("Monocitos"),
+        "monocytesTopic1": MessageLookupByLibrary.simpleMessage(
+            "Núcleo: Los monocitos tienen un núcleo grande, de forma irregular y a menudo en forma de riñón."),
+        "monocytesTopic2": MessageLookupByLibrary.simpleMessage(
+            "Color del citoplasma: El citoplasma es gris-azulado."),
+        "monocytesTopic3": MessageLookupByLibrary.simpleMessage(
+            "Tamaño y forma general: Son las células blancas más grandes y tienen una forma irregular."),
+        "monocytesTopic4": MessageLookupByLibrary.simpleMessage(
+            "Gránulos citoplasmáticos: Por lo general, no tienen gránulos visibles."),
+        "monocytesTopic5": MessageLookupByLibrary.simpleMessage(
+            "Movilidad y actividad: Son móviles y se diferencian en macrófagos cuando se desplazan a los tejidos."),
+        "moreExamples": MessageLookupByLibrary.simpleMessage("Más Ejemplos"),
+        "msg_agreeing": MessageLookupByLibrary.simpleMessage(
+            "¡Tu informe se ha guardado con éxito!"),
+        "msg_confirmCalculateCells": m0,
+        "msg_confirmClearAllValues": MessageLookupByLibrary.simpleMessage(
+            "¿Estás seguro de que quieres limpiar todos los valores?"),
+        "msg_reportDeleted":
+            MessageLookupByLibrary.simpleMessage("Informe eliminado"),
+        "neutrophilsDescription": MessageLookupByLibrary.simpleMessage(
+            "Los neutrófilos son células de defensa inmunológica que generalmente tienen núcleos segmentados, con 2 a 5 segmentos conectados por filamentos delgados. El color del citoplasma es ligeramente rosado a incoloro. Son las células blancas más abundantes, de tamaño mediano y forma redondeada a ligeramente lobulada. Pueden contener gránulos finos y pálidos, pero generalmente no son muy visibles. Los neutrófilos son altamente móviles y fagocitan bacterias."),
+        "neutrophilsTitle": MessageLookupByLibrary.simpleMessage("Neutrófilos"),
+        "neutrophilsTopic1": MessageLookupByLibrary.simpleMessage(
+            "Núcleo: Los neutrófilos generalmente tienen núcleos segmentados, con 2 a 5 segmentos conectados por filamentos delgados."),
+        "neutrophilsTopic2": MessageLookupByLibrary.simpleMessage(
+            "Color del citoplasma: El citoplasma es ligeramente rosado a incoloro."),
+        "neutrophilsTopic3": MessageLookupByLibrary.simpleMessage(
+            "Tamaño y forma general: Son las células blancas más abundantes, de tamaño mediano y forma redondeada a ligeramente lobulada."),
+        "neutrophilsTopic4": MessageLookupByLibrary.simpleMessage(
+            "Gránulos citoplasmáticos: Pueden contener gránulos finos y pálidos, pero generalmente no son muy visibles."),
+        "neutrophilsTopic5": MessageLookupByLibrary.simpleMessage(
+            "Movilidad y actividad: Los neutrófilos son altamente móviles y fagocitan bacterias."),
+        "no": MessageLookupByLibrary.simpleMessage("No"),
+        "noReportsSaved": MessageLookupByLibrary.simpleMessage(
+            "Aún no hay informes guardados"),
+        "notifications": MessageLookupByLibrary.simpleMessage("Notificaciones"),
+        "observation": MessageLookupByLibrary.simpleMessage("Observación"),
+        "openStore": MessageLookupByLibrary.simpleMessage("Abrir tienda"),
+        "optionsToSupport":
+            MessageLookupByLibrary.simpleMessage("Opciones para apoyar"),
+        "patientAge": MessageLookupByLibrary.simpleMessage("Edad del paciente"),
+        "patientName":
+            MessageLookupByLibrary.simpleMessage("Nombre del paciente"),
+        "pixKey": MessageLookupByLibrary.simpleMessage("Clave Pix"),
+        "portuguese": MessageLookupByLibrary.simpleMessage("Portugués"),
+        "quantity": MessageLookupByLibrary.simpleMessage("Cantidad"),
+        "rateUs": MessageLookupByLibrary.simpleMessage("Califícanos"),
+        "redCells": MessageLookupByLibrary.simpleMessage("Células rojas"),
+        "remove": MessageLookupByLibrary.simpleMessage("Quitar"),
+        "report": MessageLookupByLibrary.simpleMessage("Informe"),
+        "reportSaved": MessageLookupByLibrary.simpleMessage("Informe guardado"),
+        "resetConfiguration":
+            MessageLookupByLibrary.simpleMessage("Restablecer configuración"),
+        "save": MessageLookupByLibrary.simpleMessage("Guardar"),
+        "saveReport": MessageLookupByLibrary.simpleMessage(
+            "¿Quieres guardar el informe?"),
+        "savedReports":
+            MessageLookupByLibrary.simpleMessage("Informes guardados"),
+        "search": MessageLookupByLibrary.simpleMessage("Buscar"),
+        "seeReports": MessageLookupByLibrary.simpleMessage("Ver informes"),
+        "selectLanguage":
+            MessageLookupByLibrary.simpleMessage("Seleccionar Idioma"),
+        "settings": MessageLookupByLibrary.simpleMessage("Configuraciones"),
+        "shareReport":
+            MessageLookupByLibrary.simpleMessage("Compartir informe"),
+        "sound": MessageLookupByLibrary.simpleMessage("Sonido"),
+        "spanish": MessageLookupByLibrary.simpleMessage("Español"),
+        "sub_msg_confirmCalculateCells": MessageLookupByLibrary.simpleMessage(
+            "¿Quieres seguir agregando o generar el informe?"),
+        "support": MessageLookupByLibrary.simpleMessage("Apoyo"),
+        "supportOurProject":
+            MessageLookupByLibrary.simpleMessage("Apoya nuestro proyecto"),
+        "thanks":
+            MessageLookupByLibrary.simpleMessage("Mensaje de agradecimiento"),
+        "thanksMessage": MessageLookupByLibrary.simpleMessage(
+            "Gracias por usar nuestra aplicación y considerar apoyar nuestro trabajo. Tu contribución es fundamental para nuestro éxito continuo. Si tienes alguna pregunta o sugerencia, no dudes en contactarnos. ¡Juntos podemos hacer la diferencia!"),
+        "theme": MessageLookupByLibrary.simpleMessage("Tema"),
+        "tipsToIdentify":
+            MessageLookupByLibrary.simpleMessage("Consejos para identificar"),
+        "total": MessageLookupByLibrary.simpleMessage("Total"),
+        "type": MessageLookupByLibrary.simpleMessage("Tipo"),
+        "vibration": MessageLookupByLibrary.simpleMessage("Vibración"),
+        "whiteCells": MessageLookupByLibrary.simpleMessage("Células blancas"),
+        "whySupport":
+            MessageLookupByLibrary.simpleMessage("¿Por qué apoyarnos?"),
+        "yes": MessageLookupByLibrary.simpleMessage("Sí")
+      };
+}
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1287,6 +1287,9 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'es'),
+      Locale.fromSubtags(languageCode: 'fr'),
+      Locale.fromSubtags(languageCode: 'it'),
       Locale.fromSubtags(languageCode: 'pt'),
     ];
   }
